d5d870918eec77ee518e93979a22317841817bb4
Chloe Collier <achloecollier@gmail.com>
2017-09-26 19:49:09 -0700

    Looped through data via pug template

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -11,9 +11,10 @@ exports.addStore = (req, res) => {
 }
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
+  const store = await (new Store(req.body)).save(); // having await on same line lets us use store.slug
   await store.save(); //will not move on until save has successfully happened
-  res.redirect('/');
+  req.flash('success', `Successfully created ${store.name}. Care to leave review?`);
+  res.redirect(`/store/${store.slug}`);
   // for errors, we created catchErrors() - handler which will catch them for us
      /*.then(store => {
 	return Store.find(); //return all of the stores: in a list
@@ -24,4 +25,11 @@ exports.createStore = async (req, res) => {
      .catch(err => {
 	throw Error(err);
      });*/
+}
+
+exports.getStores = async (req, res) => {
+  // 1. Query the database for a list of all stores
+  const stores = await Store.find();
+  console.log(stores);
+  res.render('stores', { title: 'Stores', stores: stores });
 }
